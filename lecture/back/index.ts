79568561f7596864dd3eda8ee0d795da03fb2831
min-gukang <route2054@gmail.com>
2021-02-14 15:20:05 +0900

    set ts-node

--- lecture/back/index.ts
+++ lecture/back/index.ts
@@ -1,12 +1,13 @@
 import * as express from 'express';
 
 const app = express();
-const prod = process.env.NODE_ENV !== 'production';
+const prod = process.env.NODE_ENV === 'production';
 
+app.set('port', prod ? process.env.PORT : 3065);
 app.get('/', (req, res) => {
     res.send('react nodebird Backend normal operation');
 })
 
-app.listen(prod ? process.env.PORT : 3065, () => { //개발용은 3065, 배포용은 .env에서 자유자재로 바꿀수 있게 한다. 
-    console.log(`server is running on ${process.env.PORT}`);
-})
+app.listen(app.get('port'), () => { //개발용은 3065, 배포용은 .env에서 자유자재로 바꿀수 있게 한다. 
+    console.log(`server is running on ` + app.get('port'));
+})  
